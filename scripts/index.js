81eff2401e9a1c91788747449ac616aee2189c73
Ashish Garg <mragarg@gmail.com>
2019-03-05 13:36:04 -0500

    Adds functions for returning array of positive numbers and array of even numbers

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
 // add your solutions here
+// implementing functions without a for and while loop
+
+// positiveNumbers function takes an array of numbers and returns a new array with only positive numbers
+function positiveNumbers(array) {
+    let result = array.filter(function (num){
+        return num >= 0;
+    })
+    return result;
+}
+
+// console.log(positiveNumbers([1, 2, 3, -5, -6, -7, 10]));
+
+// evenNumbers function takes in an array of numbers and returns a new array containing only the even numbers
+function evenNumbers(array) {
+    let result = array.filter(function (num){
+        return num%2 == 0;
+    })
+    return result;
+}
+
+// console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, -8, 10]));
+
